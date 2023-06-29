b727e7c39710cd0cf00f20af289844acece9a171
joel <joel@thesportsoffice.com>
2023-06-29 11:25:20 +0100

    create gem

---
+++ lib/kitman/integrations/ussf.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "ussf/version"
+
+module Kitman
+  module Integrations
+    module Ussf
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
